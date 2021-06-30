4749de481aa89fef15f609594b6d1a37791ca8da
marijakov0804 <marija.kovacevic.0804@gmail.com>
2021-06-30 22:08:22 +0200

    http/ajax/jason exercise xml initial/demo in progress

--- http/ajax-and-jason-exercise-XML/index.js
+++ http/ajax-and-jason-exercise-XML/index.js
@@ -1,17 +1,22 @@
-// var endpoint = "http://www.geoplugin.net/xml.gp?ip=164.152.189.25";
+var endpoint = "http://www.geoplugin.net/xml.gp?ip=164.152.189.25";
+var $countryDisplay = document.querySelector("#country");
+var $submit = document.querySelector("#submit");
+var $ipInput = document.querySelector("#ip");
 // 1. Create request
-var newRequest = new XMLHttpRequets();
-// 2. Init request
-newRequest.open("GET", "http://www.geoplugin.net/xml.gp?ip=164.152.189.25");
+var newRequest = new XMLHttpRequest();
 // 3. Onload handler
 newRequest.onload = function () {
   if (newRequest.status >= 200 && newRequest.status < 300) {
     var response = newRequest.responseXML;
     var country = response.querySelector("geoplugin_countryName").textContent;
-    var $countryDisplay = document.querySelector("#country");
     $countryDisplay.textContent = country;
-    console.log(country);
   }
 };
-// 4. Send request
-newRequest.send();
+$submit.addEventListener("click", function () {
+  var ipAddress = $ipInput.value;
+  var completeURL = endpoint + ipAddress;
+  // 2. Init request
+  newRequest.open("GET", completeURL);
+  // 4. Send request
+  newRequest.send();
+});
