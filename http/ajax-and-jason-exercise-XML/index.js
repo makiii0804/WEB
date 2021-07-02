e097c88670258c4d18c79c8c81983e69432cf896
marijakov0804 <marija.kovacevic.0804@gmail.com>
2021-07-02 19:07:18 +0200

    http/ajax/jason exercise FINISHED

--- http/ajax-and-jason-exercise-XML/index.js
+++ http/ajax-and-jason-exercise-XML/index.js
@@ -1,5 +1,7 @@
-var endpoint = "http://www.geoplugin.net/xml.gp?ip=164.152.189.25";
+var endpoint = "http://www.geoplugin.net/xml.gp?ip=";
 var $countryDisplay = document.querySelector("#country");
+var $continentDisplay = document.querySelector("#continent");
+var $sityDisplay = document.querySelector("#sity");
 var $submit = document.querySelector("#submit");
 var $ipInput = document.querySelector("#ip");
 // 1. Create request
@@ -9,7 +11,13 @@ newRequest.onload = function () {
   if (newRequest.status >= 200 && newRequest.status < 300) {
     var response = newRequest.responseXML;
     var country = response.querySelector("geoplugin_countryName").textContent;
-    $countryDisplay.textContent = country;
+    var continent = response.querySelector(
+      "geoplugin_continentName"
+    ).textContent;
+    var sity = response.querySelector("geoplugin_city").textContent;
+    $countryDisplay.textContent = "Country: " + country;
+    $continentDisplay.textContent = "Continent: " + continent;
+    $sityDisplay.textContent = "Sity: " + sity;
   }
 };
 $submit.addEventListener("click", function () {
