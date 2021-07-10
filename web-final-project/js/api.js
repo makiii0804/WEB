ff630d91c48b7f93d670069c937ab75222b7b6e0
marijakov0804 <marija.kovacevic.0804@gmail.com>
2021-07-10 10:43:10 +0200

    web final project in progress

--- web-final-project/js/api.js
+++ web-final-project/js/api.js
@@ -1,34 +1,34 @@
-let endpoint = "http://api.tvmaze.com/search/shows?q=";
-let $searchInput = document.querySelector(".form-control");
-let $divContainerFluid = document.querySelector(".container-fluid");
-let request = new XMLHttpRequest();
+const endpoint = "http://api.tvmaze.com/search/shows?q=";
+const $searchInput = document.querySelector(".form-control");
+const $movieListHTML = document.querySelector("#movieList");
+const request = new XMLHttpRequest();
 request.onload = function () {
-  if (request.status >= 200 && request.status < 300) {
-    let response = JSON.parse(request.responseText);
-    console.log(response);
-    response.items.forEach(function (user) {
-      let name = user.login;
-      let imageSrc = user.image.medium;
-      let $divRow = document.createElement("div");
-      $divRow.setAttribute("class", "row");
-      $divContainerFluid.appendChild($divRow);
-      let $divCol1 = document.createElement("div");
-      $divCol1.setAttribute("class", "col");
-      $divRow.appendChild($divCol1);
-      let $divCol2 = document.createElement("div");
-      $divCol2.setAttribute("class", "col");
-      $divCol1.appendChild($divCol2);
-      let $divCard = document.createElement("div");
+  if (request.status == 200) {
+    const response = JSON.parse(request.responseText);
+    response.forEach(function (movie) {
+      console.log(movie);
+      const name = movie.name;
+      const imageSrc = movie.image.medium;
+      const $divCol = document.createElement("div");
+      $divCol.setAttribute("class", "col");
+      $movieListHTML.appendChild($divCol);
+      const $divCard = document.createElement("div");
       $divCard.setAttribute("class", "card");
-      $divCol2.appendChild($divCard);
-      let $img = document.createElement("img");
-      $name.textContent = name;
+      $divCol.appendChild($divCard);
+      const $img = document.createElement("img");
       $img.setAttribute("src", imageSrc);
-      $card.append($img, $name);
-      $userContainer.appendChild($card);
-      $result.appendChild($userContainer);
+      const $divCardBody = document.createElement("div");
+      $divCardBody.setAttribute("class", "card-body");
+      const $cardTitle = document.createElement("h5");
+      $cardTitle.setAttribute("class", "card-title");
+      $cardTitle.textContent = name;
+      $divCardBody.appendChild($cardTitle);
+      $divCard.append($img, $divCardBody);
     });
   }
 };
-request.open("GET", "http://api.tvmaze.com/shows");
-request.send();
+window.onload = function () {
+  var completeUrl = "http://api.tvmaze.com/shows";
+  request.open("GET", completeUrl);
+  request.send();
+};
