be867fc32ffecb36279f11549125b0e5ac248b22
marijakov0804 <marija.kovacevic.0804@gmail.com>
2021-07-13 18:42:39 +0200

    web final project in progress

--- web-final-project/js/api.js
+++ web-final-project/js/api.js
@@ -1,12 +1,12 @@
 const endpoint = "http://api.tvmaze.com/search/shows?q=";
 const $searchInput = document.querySelector(".form-control");
 const $movieListHTML = document.querySelector("#movieList");
+const $dropDown = document.querySelector("#dropdown");
 const request = new XMLHttpRequest();
 request.onload = function () {
   if (request.status == 200) {
     const response = JSON.parse(request.responseText);
     response.forEach(function (movie) {
-      console.log(movie);
       const name = movie.name;
       const imageSrc = movie.image.medium;
       const $divCol = document.createElement("div");
@@ -28,7 +28,30 @@ request.onload = function () {
   }
 };
 window.onload = function () {
-  var completeUrl = "http://api.tvmaze.com/shows";
+  const completeUrl = "http://api.tvmaze.com/shows";
   request.open("GET", completeUrl);
   request.send();
 };
+$searchInput.addEventListener("keyup", function () {
+  const newRequest = new XMLHttpRequest();
+  newRequest.onload = function () {
+    if (newRequest.status === 200) {
+      const searchResponse = JSON.parse(newRequest.responseText);
+      $dropDown.innerHTML = "";
+      searchResponse.forEach(function (value) {
+        console.log(value);
+        let name = value.show.name;
+        let $searchItem = document.createElement("li");
+        $searchItem.setAttribute("class", "dropdown-item");
+        $searchItem.textContent = name;
+        $dropDown.appendChild($searchItem);
+      });
+    }
+  };
+  let inputValue = $searchInput.value;
+  let newCompleteUrl = endpoint + inputValue;
+  newRequest.open("GET", newCompleteUrl);
+  newRequest.send();
+  
+});
+// kreirati event listener na input polju on keypress, unutar eventa imamo request.onload I sve te korake. u complete url ćemo imati endpoint + input.value + ona prečica za 10per page. Nakon toga pravimo foreach za lijeve unutar ul id.dropdown
