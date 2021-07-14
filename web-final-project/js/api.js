1392784325b71c6f0d7d04c9128f0b10c8dea142
marijakov0804 <marija.kovacevic.0804@gmail.com>
2021-07-14 19:23:47 +0200

    web final project in progress

--- web-final-project/js/api.js
+++ web-final-project/js/api.js
@@ -1,7 +1,10 @@
 const endpoint = "http://api.tvmaze.com/search/shows?q=";
 const $searchInput = document.querySelector(".form-control");
 const $movieListHTML = document.querySelector("#movieList");
-const $dropDown = document.querySelector("#dropdown");
+const $dropDownListWrapper = document.querySelector("#dropDownList");
+const $singleMovie = document.querySelector("#singleMovie");
+const $movieImg = document.querySelector(".movieImg");
+const $noOfSeasons = document.querySelector(".seasons");
 const request = new XMLHttpRequest();
 request.onload = function () {
   if (request.status == 200) {
@@ -33,25 +36,54 @@ window.onload = function () {
   request.send();
 };
 $searchInput.addEventListener("keyup", function () {
-  const newRequest = new XMLHttpRequest();
-  newRequest.onload = function () {
-    if (newRequest.status === 200) {
-      const searchResponse = JSON.parse(newRequest.responseText);
-      $dropDown.innerHTML = "";
-      searchResponse.forEach(function (value) {
-        console.log(value);
-        let name = value.show.name;
-        let $searchItem = document.createElement("li");
-        $searchItem.setAttribute("class", "dropdown-item");
-        $searchItem.textContent = name;
-        $dropDown.appendChild($searchItem);
-      });
-    }
-  };
-  let inputValue = $searchInput.value;
-  let newCompleteUrl = endpoint + inputValue;
-  newRequest.open("GET", newCompleteUrl);
-  newRequest.send();
-  
+  if ($searchInput.value !== "") {
+    const newRequest = new XMLHttpRequest();
+    newRequest.onload = function () {
+      if (newRequest.status === 200) {
+        const searchResponse = JSON.parse(newRequest.responseText);
+        $dropDownListWrapper.innerHTML = "";
+        const $dropDown = document.createElement("ul");
+        $dropDownListWrapper.appendChild($dropDown);
+        searchResponse.forEach(function (movie) {
+          let name = movie.show.name;
+          let $searchItem = document.createElement("li");
+          $searchItem.addEventListener("click", function replaceSingleMovie() {
+            console.log(movie);
+            let $movieId = movie.show.id;
+            $movieListHTML.style.display = "none";
+            $singleMovie.style.display = "block";
+            const movieRequest = new XMLHttpRequest();
+            movieRequest.onload = function () {
+              if (movieRequest.status === 200) {
+                const movieResponse = JSON.parse(movieRequest.responseText);
+                $movieImg.setAttribute("src", movieResponse.image.medium);
+              }
+            };
+            movieCompleteURL = "https://api.tvmaze.com/shows/" + $movieId;
+            movieRequest.open("GET", movieCompleteURL);
+            movieRequest.send();
+            const seasoneRequest = new XMLHttpRequest();
+            seasoneRequest.onload = function () {
+              if (seasoneRequest.status === 200) {
+                let seasoneResponse = JSON.parse(seasoneRequest.responseText);
+                console.log(seasoneResponse);
+                $noOfSeasons = seasoneResponse.length;
+              }
+            };
+            seasoneCompleteURL = `https://api.tvmaze.com/shows/${$movieId}/seasons`;
+            seasoneRequest.open("GET", seasoneCompleteURL);
+            seasoneRequest.send();
+          });
+          $searchItem.textContent = name;
+          $dropDown.appendChild($searchItem);
+        });
+      }
+    };
+    let inputValue = $searchInput.value;
+    let newCompleteUrl = endpoint + inputValue;
+    newRequest.open("GET", newCompleteUrl);
+    newRequest.send();
+  } else {
+    $dropDownListWrapper.innerHTML = "";
+  }
 });
-// kreirati event listener na input polju on keypress, unutar eventa imamo request.onload I sve te korake. u complete url ćemo imati endpoint + input.value + ona prečica za 10per page. Nakon toga pravimo foreach za lijeve unutar ul id.dropdown
