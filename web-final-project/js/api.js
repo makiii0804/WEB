7b0ec95238163bb255cbc7134f89f49eed946a83
marijakov0804 <marija.kovacevic.0804@gmail.com>
2021-07-15 17:58:08 +0200

    web final project FINISHED

--- web-final-project/js/api.js
+++ web-final-project/js/api.js
@@ -5,6 +5,11 @@ const $dropDownListWrapper = document.querySelector("#dropDownList");
 const $singleMovie = document.querySelector("#singleMovie");
 const $movieImg = document.querySelector(".movieImg");
 const $noOfSeasons = document.querySelector(".seasons");
+const $seasonList = document.querySelector(".seasonList");
+const $castList = document.querySelector(".castList");
+const $shDetails = document.querySelector(".details");
+const $backToHome = document.querySelector(".navbar-brand")
+
 const request = new XMLHttpRequest();
 request.onload = function () {
   if (request.status == 200) {
@@ -48,7 +53,6 @@ $searchInput.addEventListener("keyup", function () {
           let name = movie.show.name;
           let $searchItem = document.createElement("li");
           $searchItem.addEventListener("click", function replaceSingleMovie() {
-            console.log(movie);
             let $movieId = movie.show.id;
             $movieListHTML.style.display = "none";
             $singleMovie.style.display = "block";
@@ -66,24 +70,71 @@ $searchInput.addEventListener("keyup", function () {
             seasoneRequest.onload = function () {
               if (seasoneRequest.status === 200) {
                 let seasoneResponse = JSON.parse(seasoneRequest.responseText);
-                console.log(seasoneResponse);
-                $noOfSeasons = seasoneResponse.length;
+                $noOfSeasons.textContent = seasoneResponse.length;
+                $seasonList.innerHTML = "";
+                seasoneResponse.forEach(function (season) {
+                  let $seasoneData = document.createElement("li");
+                  let date1 = season.premiereDate;
+                  let date2 = season.endDate;
+                  $seasoneData.textContent = `${date1} ${date2}`;
+                  $seasonList.appendChild($seasoneData);
+                });
               }
             };
             seasoneCompleteURL = `https://api.tvmaze.com/shows/${$movieId}/seasons`;
             seasoneRequest.open("GET", seasoneCompleteURL);
             seasoneRequest.send();
+            const castRequest = new XMLHttpRequest();
+            $castList.innerHTML ="";
+            castRequest.onload = function () {
+              if (castRequest.status === 200) {
+                let castResponse = JSON.parse(castRequest.responseText);
+                castResponse.forEach(function (cast, i) {
+                  if (i < 7) {
+                    let $castData = document.createElement("li");
+                    let castName = cast.person.name;
+                    $castData.textContent = castName;
+                    $castList.appendChild($castData);
+                  }
+                });
+              }
+            };
+            castCompleteURL = `https://api.tvmaze.com/shows/${$movieId}/cast`;
+            castRequest.open("GET", castCompleteURL);
+            castRequest.send();
+            const showDetailsRequest = new XMLHttpRequest();
+            $shDetails.innerHTML = "";
+            showDetailsRequest.onload = function () {
+              if (showDetailsRequest.status === 200) {
+                let showDetailsResponse = JSON.parse(
+                  showDetailsRequest.responseText
+                  );
+                  console.log(showDetailsResponse);
+                  $shDetails.innerHTML = showDetailsResponse.summary;
+                }
+              };
+              showCompleteURL = `http://api.tvmaze.com/shows/${$movieId}`;
+              showDetailsRequest.open("GET", showCompleteURL);
+              showDetailsRequest.send();
+            });
+            $searchItem.textContent = name;
+            $dropDown.appendChild($searchItem);
           });
-          $searchItem.textContent = name;
-          $dropDown.appendChild($searchItem);
-        });
-      }
-    };
-    let inputValue = $searchInput.value;
-    let newCompleteUrl = endpoint + inputValue;
-    newRequest.open("GET", newCompleteUrl);
-    newRequest.send();
-  } else {
-    $dropDownListWrapper.innerHTML = "";
-  }
-});
+        }
+      };
+      let inputValue = $searchInput.value;
+      let newCompleteUrl = endpoint + inputValue;
+      newRequest.open("GET", newCompleteUrl);
+      newRequest.send();
+    } else {
+      $dropDownListWrapper.innerHTML = "";
+    }
+  });
+  
+  $backToHome.addEventListener("click", function() {
+    $movieListHTML.style.display = "flex";
+    $singleMovie.style.display = "none";
+    request.send();
+    
+  })
+  // kreirati event listener na input polju on keypress, unutar eventa imamo request.onload I sve te korake. u complete url ćemo imati endpoint + input.value + ona prečica za 10per page. Nakon toga pravimo foreach za lijeve unutar ul id.dropdown
